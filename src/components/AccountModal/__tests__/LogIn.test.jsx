19a56088c85eff6dcef5d864a41cdf09ab711d44
daniil.zavadin <daniil.zavadin@gmail.com>
2021-06-15 22:52:48 +0300

    tests finished

--- src/components/AccountModal/__tests__/LogIn.test.jsx
+++ src/components/AccountModal/__tests__/LogIn.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import LogIn from '../modalComponents/LogIn';
 
@@ -8,12 +8,22 @@ describe('Log In test', () => {
     render(<LogIn />);
     expect(screen.getByTestId('form')).toBeInTheDocument();
   });
-  it('should call submit with values', () => {
+  it('should call submit with values', async () => {
     const submit = jest.fn();
     render(<LogIn handleSubmit={submit} handleBottomLink={jest.fn()} />);
     userEvent.type(screen.getByTestId('loginOrEmail'), 'testLogin');
     userEvent.type(screen.getByTestId('login-password'), '123123123123');
     userEvent.click(screen.getByTestId('account-modal-checkbox'));
     userEvent.click(screen.getByTestId('account-modal-button'));
+    await waitFor(() =>
+      expect(submit).toHaveBeenCalledWith(
+        {
+          loginOrEmail: 'testLogin',
+          password: '123123123123',
+          accept: true,
+        },
+        expect.anything()
+      )
+    );
   });
 });
--- src/components/AccountModal/__tests__/SignUp.test.jsx
+++ src/components/AccountModal/__tests__/SignUp.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SignUp from '../modalComponents/SignUp';
 
@@ -8,8 +8,9 @@ describe('account modal test', () => {
     render(<SignUp />);
     expect(screen.getByTestId('form')).toBeInTheDocument();
   });
-  it('should call submit with values', () => {
-    render(<SignUp />);
+  it('should call submit with values', async () => {
+    const submit = jest.fn();
+    render(<SignUp handleSubmit={submit} handleBottomLink={jest.fn()} />);
     userEvent.type(screen.getByTestId('input-name'), 'Test Test');
     userEvent.type(screen.getByTestId('input-login'), 'testLogin');
     userEvent.type(screen.getByTestId('input-email'), 'test@test.com');
@@ -17,5 +18,18 @@ describe('account modal test', () => {
     userEvent.type(screen.getByTestId('confirm-password'), '123123123123');
     userEvent.click(screen.getByTestId('account-modal-checkbox'));
     userEvent.click(screen.getByTestId('account-modal-button'));
+    await waitFor(() =>
+      expect(submit).toHaveBeenCalledWith(
+        {
+          name: 'Test Test',
+          login: 'testLogin',
+          email: 'test@test.com',
+          password: '123123123123',
+          passwordconf: '123123123123',
+          accept: true,
+        },
+        expect.anything()
+      )
+    );
   });
 });
