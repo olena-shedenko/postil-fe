8b145a775262fd2b864bc73318e1618e6588c40a
PC Boyarin <dmitriytsua@gmail.com>
2021-07-01 19:11:09 +0300

    fixed some problems and finished shipping details

--- src/components/CatalogItem/CatalogItem.jsx
+++ src/components/CatalogItem/CatalogItem.jsx
@@ -55,5 +55,5 @@ CatalogItem.propTypes = {
   name: PropTypes.string.isRequired,
   currentPrice: PropTypes.number.isRequired,
   itemNo: PropTypes.string.isRequired,
-  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  items: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
